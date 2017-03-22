4d2ece7e94e8ca187c3dc95b9ae88291059159dc
Wilson Wong <wilsonwong13@gmail.com>
2017-03-22 12:21:23 -0400

    udpate 12:21

--- public/js/main.js
+++ public/js/main.js
@@ -48,7 +48,7 @@ $(function initializeMap (){
     activity: '/images/star-3.png'
   };
 
-  function drawMarker (type, coords) {
+  function drawMarker (type, coords, name) {
     var latLng = new google.maps.LatLng(coords[0], coords[1]);
     var iconURL = iconURLs[type];
     var marker = new google.maps.Marker({
@@ -56,14 +56,18 @@ $(function initializeMap (){
       position: latLng
     });
     marker.setMap(currentMap);
+    hashOfMarker[name] = marker
   }
+
+  var hashOfMarker = {}
+  var days = {1:[[],[],[]]}
+  var currentDay = 1; 
 
   // drawMarker('hotel', [40.705137, -74.007624]);
   // drawMarker('restaurant', [40.705137, -74.013940]);
   // drawMarker('activity', [40.716291, -73.995315]);
-
-});
-
+  
+  // Adding names to the list 
   hotels.forEach(function(element){
     $('#hotel-choices').append('<option>'+element.name+'</option>')
   })
@@ -76,12 +80,75 @@ $(function initializeMap (){
     $('#activity-choices').append('<option>'+element.name+'</option>')
   })
 
-  $('#options-panel').on('click', 'button', function() {
-    var hotelname = $('#hotel-choices').find(':selected').val()
-    for (var i = 0; i < hotels.length; i ++) {
-      if(hotels[i].name === hotelname) {
-        console.log('this is the hotel name', hotels[i].place)
-        console.log('howdy')
+  //Adding to the list 
+  $('#options-panel div:nth-child(1)').on('click', 'button', function() {
+    var hotelName = $('#hotel-choices option:selected').text()
+    for(var i = 0; i < hotels.length; i ++) {
+      if(hotels[i].name === hotelName) {
+        drawMarker('hotel', hotels[i].place.location, hotelName);
+        $("h4:contains('My Hotel') + >" ).append(`<span class="title">${hotelName}</span>
+          <button class="btn btn-xs btn-danger remove btn-circle">x</button>`)
+        days[currentDay][0].push(hotels[i])
+        
       }
-    } 
+    }
   })
+  
+  $('#options-panel div:nth-child(2)').on('click', 'button', function() {
+    var restaurantsName = $('#restaurant-choices option:selected').text()
+     for(var i = 0; i < restaurants.length; i ++) {
+      if(restaurants[i].name === restaurantsName) {
+        drawMarker('restaurant', restaurants[i].place.location, restaurantsName);
+        $("h4:contains('My Restaurants') + >" ).append(`<span class="title">${restaurantsName}</span>
+          <button class="btn btn-xs btn-danger remove btn-circle">x</button>`)
+          days[currentDay][1].push(restaurants[i])       
+      }
+    }   
+  })
+  $('#options-panel div:nth-child(3)').on('click', 'button', function() {
+    var activityName = $('#activity-choices option:selected').text()
+     for(var i = 0; i < activities.length; i ++) {
+      if(activities[i].name === activityName) {
+        drawMarker('activity', activities[i].place.location, activityName);
+        $("h4:contains('My Activities') + >" ).append(`<span class="title">${activityName}</span>
+          <button class="btn btn-xs btn-danger remove btn-circle">x</button>`)  
+        days[currentDay][2].push(activities[i])
+      }
+    }
+
+  }) 
+
+  //Removing from the list 
+  $('#itinerary').on('click', '.remove', function() {
+    var upperSibling = $(this).prev();
+    upperSibling.remove();
+    $(this).remove();
+    (hashOfMarker[upperSibling.text()]).setMap(null)
+    
+  })
+
+  //Adding A Date 
+  $('.day-buttons').on('click', '#day-add', function(){
+    var prevValue = parseInt($(this).prev().text())
+    $(this).before(`<button class="btn btn-circle day-btn">${prevValue + 1}</button>`)
+    days[prevValue + 1] = [[],[],[]];
+    ///console.log(days)
+  })
+
+  //Current Date
+  $('.day-buttons').on('click', '.day-btn', function() {
+    if($(this).text() != '+') {
+      $('.current-day').removeClass('current-day')
+      $(this).addClass('current-day');
+      $('#day-title span:nth-child(1)').text(`Day `+ $(this).text())
+      currentDay = $('.current-day').text()
+      console.log(days)
+    }
+  })
+
+});
+
+
+
+
+
